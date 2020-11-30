4ac85316985d9117671bb78f4f440813da3ab640
Maksym Kozyrenko <maksymkozyrenko@Maksym.Kozyrenko>
2020-11-30 16:50:00 +0200

    add homework

---
+++ Main.java
@@ -0,0 +1,39 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter a month");
+        String month = br.readLine();
+
+        switch (month) {
+            case "December":
+            case "Jenuary":
+            case "February":
+                System.out.println("Winter!");
+                break;
+            case "March":
+            case "April":
+            case "May":
+                System.out.println("Spring");
+                break;
+            case "June":
+            case "July":
+            case "August":
+                System.out.println("Summer");
+                break;
+            case "September":
+            case "October":
+            case "November":
+                System.out.println("Autumn");
+                break;
+
+        }
+
+    }
+}
